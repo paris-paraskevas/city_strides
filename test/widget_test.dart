4767e8bbd93afb18d8048459442768857ae0a8a1
paris-paraskevas <businessofzeus@gmail.com>
2026-02-12 15:46:56 +0200

    trynna fix this shit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:city_strides/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CityStridesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
